26471468d94916717e8f7dad60be4caab4bce42f
eraysemiz <137297567+eraysemiz@users.noreply.github.com>
2023-09-02 20:16:10 +0300

    Add files via upload

---
+++ 6kyu/Detect Pangram.py
@@ -0,0 +1,8 @@
+def is_pangram(s):
+    alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+                "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    for i in alphabet:
+        if i not in s.upper():
+            return False
+    return True
+
